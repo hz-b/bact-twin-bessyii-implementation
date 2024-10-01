447dfbe326939b0cea8cada6af710eb55e06ade0
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2024-10-01 18:37:35 +0200

    [TASK] matching lattice elem prop to dev prop, unit conversion repo now data model of architecture

--- src/bact_twin_bessyii_impl/bl/element_property_tranformer.py
+++ src/bact_twin_bessyii_impl/bl/element_property_tranformer.py
@@ -1,9 +1,10 @@
 from typing import Tuple
 
+from bact_twin_architecture.data_model.identifiers import DevicePropertyID, LatticeElementPropertyID
 from bact_twin_architecture.interfaces.id_property_transformer import (
     IdentifierPropertyTransformerBase,
 )
-from bact_twin_architecture.interfaces.identifier import DeviceIdentifier
+# from bact_twin_architecture.interfaces.identifier import DeviceIdentifier
 
 
 class IdentifierPropertyTransformer(IdentifierPropertyTransformerBase):
@@ -16,11 +17,11 @@ class IdentifierPropertyTransformer(IdentifierPropertyTransformerBase):
         Define interface class for it
     """
 
-    def forward(self, id: str, property: str) -> Tuple[DeviceIdentifier, str]:
-        if property == "x_kick":
-            return "current"
-        elif property == "y_kick":
-            return "current"
+    def forward(self, id_: LatticeElementPropertyID) -> DevicePropertyID:
+        if id_.property == "x_kick":
+            return DevicePropertyID(device_name=None, property="current")
+        elif id_.property == "y_kick":
+            return DevicePropertyID(device_name=None, property="current")
         else:
             raise NotImplementedError(f"not handling {property}. I am a hack anyway")
 

e2719e0515c06f6302fb28f8b7c9cf0284f4bef4
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2024-10-01 12:31:37 +0200

    [Snapshot] cmds rewritten

--- examples/execute_orm_commands.py
+++ examples/execute_orm_commands.py
@@ -3,15 +3,18 @@ from bact_twin_architecture.data_model.command import Command, CommandSequence
 from bact_twin_bessyii_impl.bl.bluesky_measurement_execution_engine import BlueskyMeasurementExecutionEngine
 from bact_twin_bessyii_impl.bl.io.pytac_repositories import PyTACRepository
 from bact_twin_bessyii_impl.bl.state_conversion import UnitConversionRepo, UnitConversionFacade
-from ophyd import Component as Cpt, EpicsSignal, EpicsSignalRO, PVPositionerPC
+from ophyd import Component as Cpt, Device, DynamicDeviceComponent, EpicsSignal, EpicsSignalRO, PVPositionerPC
 from bluesky.run_engine import RunEngine
 
 repo = PyTACRepository()
 
-
-class Steerer(PVPositionerPC):
+class SteererCurrent(PVPositionerPC):
     setpoint = Cpt(EpicsSignal, ":set")
     readback = Cpt(EpicsSignalRO, ":set")
+
+
+class Steerer(Device):
+    current = Cpt(SteererCurrent, "", name="cur")
 
 
 with open("orm_commands.json") as fp:
@@ -23,5 +26,12 @@ transformer = UnitConversionFacade(
 cmds_on_machine = CommandSequence(commands=[
     transformer.command_rewrite_forward(cmd) for cmd in cmds_on_lattice.commands
 ])
+cmds_on_machine
+device_ids = set([cmd.id for cmd in cmds_on_machine.commands])
+
+class SteererCollection:
+    steerers = DynamicDeviceComponent(device_ids)
+    pass
+
 # RE = bluesky.run_engine.RunEngine()
 # mexec = BlueskyMeasurementExecutionEngine(run_engine=RE, devices=)
--- src/bact_twin_bessyii_impl/bl/state_conversion.py
+++ src/bact_twin_bessyii_impl/bl/state_conversion.py
@@ -57,13 +57,38 @@ class UnitConversionRepo:
         obj =  lut[(pos_name, property)]
         return obj
 
+
+class PropertyRenamer:
+    """I guess that will not be that simple
+
+    Todo:
+        implement it properrly!
+        Should map id, property to id, property
+    """
+    def forward(self, id: str, property: str) -> str:
+        if property == "x_kick":
+            return "current"
+        elif property == "y_kick":
+            return "current"
+        else:
+            raise NotImplementedError(f"not handling {property}. I am hack anyway")
+
 class UnitConversionFacade:
+    """
+    Todo:
+        split it up in different objects?
+        seems to have more than one responsibility
+    """
     def __init__(self, unit_conversion_repo : UnitConversionRepo):
         """create the factory based on the repo that reads in the pytac files
         """
         self.unit_conversion_repo = unit_conversion_repo
+        self.property_renamer = PropertyRenamer()
 
-    def get(self, id: Identifier, property: str) -> UnitConversion:
+    def get_conversion_info(self, id: Identifier, property: str) -> LinearUnitConversionInfo:
+        return self.unit_conversion_repo.get(id, property)
+
+    def get_converter(self, id: Identifier, property: str) -> UnitConversion:
         """
         Todo:
             revisit to be far more flexible: not only linear conversion
@@ -81,17 +106,22 @@ class UnitConversionFacade:
 
             These then start to mix
         """
-        return self.get(id, property).forward(value)
+        return self.get_converter(id, property).forward(value)
 
     def update_inverse(self, id: Identifier, property: str, value: float) -> float:
-        return self.get(id, property).inverse(value)
-
-
+        return self.get_converter(id, property).inverse(value)
 
     def command_rewrite_forward(self, cmd : Command) -> Command:
-        return Command(
-            id=cmd.id,
-            property=cmd.property,
-            value=self.update_forward(cmd.id, cmd.property, cmd.value),
+        """
+        Todo:
+            just take it out and make it a function?
+        """
+        info = self.get_conversion_info(cmd.id, cmd.property)
+        assert info.device_name is not None
+        ncmd = Command(
+            id=info.device_name,
+            property=self.property_renamer.forward(id=cmd.id, property=cmd.property),
+            value=self.update_forward(id=cmd.id, property=cmd.property, value=cmd.value),
             behaviour_on_error=cmd.behaviour_on_error
         )
+        return ncmd

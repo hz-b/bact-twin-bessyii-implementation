57459d8cc58fcf3b265af48d6799ff8f963652b0
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2025-02-17 15:42:56 +0100

    [TASK] clean up of code

--- tests/test_pytac_repository_load.py
+++
@@ -1,8 +0,0 @@
-from bact_twin_bessyii_impl.bl.io.pytac_repositories import PyTACRepository
-
-
-def test_pytac_repo_load():
-    """just check that it loads the repositories"""
-    repo = PyTACRepository()
-    repo.device_repo
-    repo.family_repo
